69a54c40198b08bcbf5592c9bb600380c0f75921
dani01Lost4ever <daniel.busetto.db@gmail.com>
2023-11-24 12:52:31 +0100

    Second Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,61 @@
 package org.example;
+import java.util.Scanner;
+import java.io.IOException;
+import java.sql.*;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Invio with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
 
-        // Press Maiusc+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        try{
 
-            // Press Maiusc+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            System.out.println("Hello and welcome!".substring(0, 5));
+            DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
+            String dburl ="jdbc:sqlserver://localhost;databaseName=Northwind;integratedSecurity=true;encrypt=true;trustServerCertificate=true;";
+            Connection connection = DriverManager.getConnection(dburl);
+            if(connection!= null){
+                System.out.println("Connected");
+
+                System.out.println("Scrivere 0 per stampare, 1 per inserire");
+                int scelta = System.in.read();
+                Statement statement = connection.createStatement();
+                switch(scelta) {
+                    case '0':
+                        // Code to execute when scelta is 0
+                        String sql = "select * from Categories";
+                        ResultSet resultSet = statement.executeQuery(sql);
+                        while(resultSet.next()){
+                            System.out.println(resultSet.getString("CategoryID") + " " + resultSet.getString("CategoryName")+ " " + resultSet.getString("Description"));
+                        }
+                        break;
+                    case '1':
+                        // Code to execute when scelta is 1
+                        Scanner scanner = new Scanner(System.in);
+                        String temp = scanner.nextLine();
+                        System.out.println("Enter a CategoryName:");
+                        String category = scanner.nextLine();
+                        System.out.println("Enter a Description:");
+                        String description = scanner.nextLine();
+                        String insertSql2 = "insert into Categories(CategoryName, Description) values('"+category+"', '"+description+"')";
+                        ResultSet result = statement.executeQuery(insertSql2);
+                        System.out.println("done");
+                        break;
+                    default:
+                        // Code to execute when scelta does not match any cases
+                        System.out.println("Invalid choice");
+                        break;
+                }
+                connection.close();
+            }
+            else{
+                System.out.println("Not Connected");
+            }
+
+        } catch (SQLException | IOException e) {
+
+            System.out.println("Exception: " + e.getMessage());
         }
     }
 }
